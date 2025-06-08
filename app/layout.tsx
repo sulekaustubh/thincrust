054d9edc76372e63186034e37a0703907444e093
kaustubh-GG <sulekaustubh97@gmail.com>
2025-06-09 01:56:31 +0530

    Outfit font installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const outfit = Outfit({
+	variable: "--font-outfit",
 	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const geistMono = Geist_Mono({
@@ -29,7 +30,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#060818]`}
+				className={`${outfit.variable} ${geistMono.variable} antialiased bg-[#111010]`}
 			>
 				{children}
 			</body>
